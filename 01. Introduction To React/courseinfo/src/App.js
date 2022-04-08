7198ca68491b579d1ddb70101e870f44b4e72c38
Eric Chin <ericychin94@gmail.com>
2022-04-08 13:30:11 -0700

    Created a separate Header component to display the course name

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -1,5 +1,9 @@
-const Header = () => {
-
+const Header = (props) => {
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+    );
 };
 
 const Content = () => {
@@ -21,7 +25,7 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
+            <Header course={course} />
             <p>
                 {part1} {exercises1}
             </p>
