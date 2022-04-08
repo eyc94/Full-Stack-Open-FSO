510d69b4b4ff290aadba15c800c49fd03bb19a58
Eric Chin <ericychin94@gmail.com>
2022-04-08 13:34:20 -0700

    Created a Total component that displays the sum of all exercise numbers

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -22,8 +22,12 @@ const Content = (props) => {
     );
 };
 
-const Total = () => {
-
+const Total = (props) => {
+    return (
+        <>
+            <p>Number of exercises {props.total}</p>
+        </>
+    );
 };
 
 const App = () => {
@@ -39,7 +43,7 @@ const App = () => {
         <div>
             <Header course={course} />
             <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     );
 };
