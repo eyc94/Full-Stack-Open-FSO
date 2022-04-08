1866a20ba815a411444c0ecbdcae197429b83ac7
Eric Chin <ericychin94@gmail.com>
2022-04-08 13:32:37 -0700

    Created a Content component that handles displaying the part name and its number of exercises

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -6,8 +6,20 @@ const Header = (props) => {
     );
 };
 
-const Content = () => {
-
+const Content = (props) => {
+    return (
+        <>
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+            <p>
+                {props.part2} {props.exercises2}
+            </p>
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+        </>
+    );
 };
 
 const Total = () => {
@@ -26,15 +38,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
             <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
         </div>
     );
