c5a939ab8392a3113ed38d07548ca6ed9ad5f29c
Eric Chin <ericychin94@gmail.com>
2022-04-10 17:54:06 -0700

    Removed exercises input value in Content component

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -53,7 +53,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content part1={part1} part2={part2} part3={part3} />
             <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     );
