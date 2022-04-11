2642a2d4edc3bf0afc4993fb90230d749b805b7d
Eric Chin <ericychin94@gmail.com>
2022-04-10 17:55:26 -0700

    Fixed Content rendering and changed Total component input values

--- 01. Introduction To React/courseinfo/src/App.js
+++ 01. Introduction To React/courseinfo/src/App.js
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
+            <Part part={props.part1.name} exercises={props.part1.exercises} />
+            <Part part={props.part2.name} exercises={props.part2.exercises} />
+            <Part part={props.part3.name} exercises={props.part3.exercises} />
         </>
     );
 };
@@ -54,7 +54,7 @@ const App = () => {
         <div>
             <Header course={course} />
             <Content part1={part1} part2={part2} part3={part3} />
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Total total={part1.exercises + part2.exercises + part3.exercises} />
         </div>
     );
 };
