4fd5f23a3c9b0e680adb73dd68f2559b01aa2d64
Eric Chin <ericychin94@gmail.com>
2022-04-13 19:54:04 -0700

    Added decreaseByOne function and used Button component with event handler functions passed into them

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -4,17 +4,24 @@ const App = () => {
     const [counter, setCounter] = useState(0);
 
     const increaseByOne = () => setCounter(counter + 1);
+    const decreaseByOne = () => setCounter(counter - 1);
     const setToZero = () => setCounter(0);
 
     return (
         <div>
             <Display counter={counter} />
-            <button onClick={increaseByOne}>
-                plus
-            </button>
-            <button onClick={setToZero}>
-                zero
-            </button>
+            <Button
+                onClick={increaseByOne}
+                text="plus"
+            />
+            <Button
+                onClick={setToZero}
+                text="zero"
+            />
+            <Button
+                onClick={decreaseByOne}
+                text="minus"
+            />
         </div>
     );
 };
