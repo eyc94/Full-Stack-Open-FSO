d8e746adbe1d4612e3d9c6a4e3ce34f068c23caa
Eric Chin <ericychin94@gmail.com>
2022-04-13 19:14:48 -0700

    Reverted index.js back to original

--- 01. Introduction To React/part1/src/index.js
+++ 01. Introduction To React/part1/src/index.js
@@ -2,17 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-let counter = 1;
-
-const refresh = () => {
-    ReactDOM.render(
-        <App counter={counter} />,
-        document.getElementById('root')
-    );
-};
-
-refresh();
-counter += 1;
-refresh();
-counter += 1;
-refresh();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
