8ae6b9379b7dc27b0716b47562023b3eb5f20b2a
Eric Chin <ericychin94@gmail.com>
2022-04-13 20:15:45 -0700

    Removed old commented code

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -36,29 +36,4 @@ const Button = ({ onClick, text }) => {
     );
 };
 
-// const Hello = ({ name, age }) => {
-//     const bornYear = () => new Date().getFullYear() - age;
-
-//     return (
-//         <div>
-//             <p>Hello {name}, you are {age} years old</p>
-//             <p>So you were probably born in {bornYear()}</p>
-//         </div>
-//     );
-// };
-
-// Before the counter implementation.
-// const App = () => {
-//     const name = "Peter";
-//     const age = 10;
-
-//     return (
-//         <div>
-//             <h1>Greetings</h1>
-//             <Hello name="Maya" age={26 + 10} />
-//             <Hello name={name} age={age} />
-//         </div>
-//     );
-// };
-
 export default App;
