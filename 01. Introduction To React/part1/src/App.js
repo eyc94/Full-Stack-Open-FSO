4bae04c2412ddfcc3cba3b1edd7840eaad029ea7
Eric Chin <ericychin94@gmail.com>
2022-04-13 19:27:07 -0700

    Removed old event handling function

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -3,10 +3,6 @@ import { useState } from 'react';
 const App = () => {
     const [counter, setCounter] = useState(0);
 
-    const handleClick = () => {
-        console.log("clicked");
-    }
-
     return (
         <div>
             <div>{counter}</div>
