f457149db6ed5f7c97a6ce0bf4954222cb0fd87f
Eric Chin <ericychin94@gmail.com>
2022-04-13 19:13:39 -0700

    Made counter incrementing possible with instant rerendering

--- 01. Introduction To React/part1/src/index.js
+++ 01. Introduction To React/part1/src/index.js
@@ -4,7 +4,15 @@ import App from './App';
 
 let counter = 1;
 
-ReactDOM.render(
-    <App counter={counter} />,
-    document.getElementById('root')
-);
+const refresh = () => {
+    ReactDOM.render(
+        <App counter={counter} />,
+        document.getElementById('root')
+    );
+};
+
+refresh();
+counter += 1;
+refresh();
+counter += 1;
+refresh();
