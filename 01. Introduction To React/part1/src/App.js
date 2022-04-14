2cfc0779ea8902cce5500e8d5bd5cdf61414ca4e
Eric Chin <ericychin94@gmail.com>
2022-04-13 19:16:29 -0700

    Commented old code and set state to the App counter for a counter that increments every second

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,20 +1,28 @@
-const Hello = ({ name, age }) => {
-    const bornYear = () => new Date().getFullYear() - age;
+import { useState } from 'react';
 
-    return (
-        <div>
-            <p>Hello {name}, you are {age} years old</p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
+const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
     );
-};
 
-const App = (props) => {
-    const { counter } = props;
     return (
         <div>{counter}</div>
     );
 };
+
+// const Hello = ({ name, age }) => {
+//     const bornYear = () => new Date().getFullYear() - age;
+
+//     return (
+//         <div>
+//             <p>Hello {name}, you are {age} years old</p>
+//             <p>So you were probably born in {bornYear()}</p>
+//         </div>
+//     );
+// };
 
 // Before the counter implementation.
 // const App = () => {
