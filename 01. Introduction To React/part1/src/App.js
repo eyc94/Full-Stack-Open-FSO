59debb25fa4df1ba20b47f1c8a81a9368427a40e
Eric Chin <ericychin94@gmail.com>
2022-04-15 10:26:25 -0700

    Added conditional rendering for the history

--- 01. Introduction To React/part1/src/App.js
+++ 01. Introduction To React/part1/src/App.js
@@ -1,5 +1,21 @@
 import { useState } from 'react';
 
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                The app is used by pressing the buttons
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            Button press history: {props.allClicks.join(' ')}
+        </div>
+    );
+};
+
 const App = () => {
     const [left, setLeft] = useState(0);
     const [right, setRight] = useState(0);
@@ -21,7 +37,7 @@ const App = () => {
             <button onClick={handleLeftClick}>left</button>
             <button onClick={handleRightClick}>right</button>
             {right}
-            <p>{allClicks.join(' ')}</p>
+            <History allClicks={allClicks} />
         </div>
     );
 };
