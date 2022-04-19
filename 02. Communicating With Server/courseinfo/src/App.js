0d7f7432427d81838be3ee9fd173bb2cf3d8d048
Eric Chin <ericychin94@gmail.com>
2022-04-19 02:35:09 -0700

    Used the array map() method to create Content Parts regardless of whether the parts array is changing or not

--- 02. Communicating With Server/courseinfo/src/App.js
+++ 02. Communicating With Server/courseinfo/src/App.js
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+            {props.parts.map(part =>
+                <Part key={part.id} part={part.name} exercises={part.exercises} />
+            )}
         </>
     );
 };
