dcc9254f0b2e2db701e91a537fc5a1586ed786b4
Eric Chin <ericychin94@gmail.com>
2022-04-19 02:30:39 -0700

    Added Course component and fixed code to handle new types of props

--- 02. Communicating With Server/courseinfo/src/App.js
+++ 02. Communicating With Server/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 const Header = (props) => {
     return (
         <>
-            <h1>{props.course.name}</h1>
+            <h1>{props.name}</h1>
         </>
     );
 };
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-            <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-            <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+            <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
         </>
     );
 };
@@ -29,6 +29,15 @@ const Total = (props) => {
         <>
             <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
         </>
+    );
+};
+
+const Course = (props) => {
+    return (
+        <div>
+            <Header name={props.course.name} />
+            <Content parts={props.course.parts} />
+        </div>
     );
 };
 
@@ -56,14 +65,6 @@ const App = () => {
     };
 
     return <Course course={course} />;
-
-    // return (
-    //     <div>
-    //         <Header course={course} />
-    //         <Content course={course} />
-    //         <Total course={course} />
-    //     </div>
-    // );
 };
 
 export default App;
