044973663177272aae8cab752de38eb1e6b8e25d
Eric Chin <ericychin94@gmail.com>
2022-04-19 03:12:31 -0700

    Made courses be an array

--- 02. Communicating With Server/courseinfo/src/App.js
+++ 02. Communicating With Server/courseinfo/src/App.js
@@ -47,27 +47,45 @@ const Course = (props) => {
 };
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack Application Development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3
-            }
-        ]
-    };
+    const course = [
+        {
+            id: 1,
+            name: "Half Stack Application Development",
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: "Node.js",
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ];
 
     return <Course course={course} />;
 };
