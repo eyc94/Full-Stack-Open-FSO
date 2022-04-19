d27267f706050e3d08ac105422efe3157768e3cf
Eric Chin <ericychin94@gmail.com>
2022-04-19 03:30:16 -0700

    Removed subcomponents of Course and placed them into new module

--- 02. Communicating With Server/courseinfo/src/components/Course.js
+++ 02. Communicating With Server/courseinfo/src/components/Course.js
@@ -14,4 +14,30 @@ const Course = (props) => {
     );
 };
 
+const Header = (props) => {
+    return (
+        <>
+            <h2>{props.name}</h2>
+        </>
+    );
+};
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
+    );
+};
+
+const Content = (props) => {
+    return (
+        <>
+            {props.parts.map(part =>
+                <Part key={part.id} part={part.name} exercises={part.exercises} />
+            )}
+        </>
+    );
+};
+
 export default Course;
