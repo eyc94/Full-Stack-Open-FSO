1e18cc785d04af4e443d40dd928755caa8eaa058
Eric Chin <ericychin94@gmail.com>
2022-04-19 03:29:41 -0700

    Removed Course from App and placed in new module

--- 02. Communicating With Server/courseinfo/src/components/Course.js
+++ 02. Communicating With Server/courseinfo/src/components/Course.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Course = (props) => {
-
+    return (
+        <>
+            {props.courses.map(course =>
+                <div key={course.id}>
+                    <Header name={course.name} />
+                    <Content parts={course.parts} />
+                    <strong><Total parts={course.parts} /></strong>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Course;
