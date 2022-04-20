79d6c0efc4835969fa366564addcce45641e7597
Eric Chin <ericychin94@gmail.com>
2022-04-20 16:57:42 -0700

    Used person's name as the key attribute's value

--- 02. Communicating With Server/phonebook/src/App.js
+++ 02. Communicating With Server/phonebook/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {persons.map(person =>
-                    <div>{person.name}</div>
+                    <div key={person.name}>{person.name}</div>
                 )}
             </div>
         </div>
