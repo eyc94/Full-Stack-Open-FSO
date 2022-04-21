61752e094458bacf5e254b2ea66cd9df2e7cc523
Eric Chin <ericychin94@gmail.com>
2022-04-20 18:23:42 -0700

    Added more hard-coded dummy code

--- 02. Communicating With Server/phonebook/src/App.js
+++ 02. Communicating With Server/phonebook/src/App.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: "Arto Hellas",
-            number: "040-1234567"
-        }
+        { name: "Arto Hellas", number: "040-1234567" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" }
     ]);
+
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
 
