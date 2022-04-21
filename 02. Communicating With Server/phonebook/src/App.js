c3a81332f35ded91c4a5c50b8894971573a5c3a2
Eric Chin <ericychin94@gmail.com>
2022-04-20 17:12:15 -0700

    Displaying numbers alongside names

--- 02. Communicating With Server/phonebook/src/App.js
+++ 02. Communicating With Server/phonebook/src/App.js
@@ -51,7 +51,7 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {persons.map(person =>
-                    <div key={person.name}>{person.name}</div>
+                    <div key={person.name}>{person.name} {person.number}</div>
                 )}
             </div>
         </div>
