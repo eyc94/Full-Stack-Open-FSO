b554e1c0e8f9edaeb0e7f4a64dfaae6ed54cff9d
Eric Chin <ericychin94@gmail.com>
2022-04-20 18:47:52 -0700

    Created Persons module

---
+++ 02. Communicating With Server/phonebook/src/components/Persons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Persons = (props) => {
+    return (
+        <div>
+            {props.persons.map(person =>
+                <div key={person.id}>{person.name} {person.number}</div>
+            )}
+        </div>
+    );
+};
+
+export default Persons;
