eb41d9303ef622ded287adffba17d865054735e5
Eric Chin <ericychin94@gmail.com>
2022-04-20 18:24:42 -0700

    Added id property to person dummy code objects

--- 02. Communicating With Server/phonebook/src/App.js
+++ 02. Communicating With Server/phonebook/src/App.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-1234567" },
-        { name: "Ada Lovelace", number: "39-44-5323523" },
-        { name: "Dan Abramov", number: "12-43-234345" },
-        { name: "Mary Poppendieck", number: "39-23-6423122" }
+        { name: "Arto Hellas", number: "040-1234567", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
     ]);
 
     const [newName, setNewName] = useState('');
